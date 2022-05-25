efacd2a6d02ad0d62e490999287e0a1c57629955
nayong2021 <nayong2017@ajou.ac.kr>
2022-05-26 06:00:30 +0900

    practiced implementing image cropping

---
+++ CropPractice/app/src/main/java/kr/co/company/croppractice/MainActivity.java
@@ -0,0 +1,21 @@
+package kr.co.company.croppractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(new SomeView(MainActivity.this));
+    }
+
+}
